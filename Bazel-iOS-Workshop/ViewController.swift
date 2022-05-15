9cea3bb63b2a11882a36a6682f7db5a03434ed31
Samuel Giddins <segiddins@squareup.com>
2022-05-15 23:16:19 +0200

    Step 10 - Swift App

---
+++ Bazel-iOS-Workshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bazel-iOS-Workshop
+//
+//  Created by Samuel Giddins on 5/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
